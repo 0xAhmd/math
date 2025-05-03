71fba59af805d2fd093e20eac0699dc80019059d
0xAhmd <ahmedhesham200987@gmail.com>
2025-05-03 18:42:57 +0300

    fix: correct import path for home_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:math/pages/home_page.dart';
+
+import 'pages/home_page.dart';
 
 //
 void main() {
